2d22b304f34bfb19f10a406fc5a29bdf6e3ea5c6
Parabeac-Nest <admin@parabeac.com>
2022-11-05 08:18:26 +0000

    Auto-generated commit by Parabeac Cloud

---
+++ lib/theme/test_colors.g.dart
@@ -0,0 +1,38 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';
+class TestColors {
+
+static const Color neutralColorsWhite = Color(0xffffffff);
+
+static const Color neutralColorsSoftGrey = Color(0xff929292);
+
+static const Color neutralColorsChacoral = Color(0xff000000);
+
+static const Color primaryColorVàngUnitel = Color(0xfff68b1f);
+
+static const Color iconOrange = Color(0xffed5e0d);
+
+static const Color iconRed = Color(0xffc6110c);
+
+static const Color iconDeepYellow = Color(0xfffc9d00);
+
+static const Color iconLightYellow = Color(0xffffe5b6);
+
+static const Color iconWhite = Color(0xffffffff);
+
+static const Color primary#14AFB4 = Color(0xff14afb4);
+
+static const Color primaryColorVàngUnitelĐậm = Color(0xfff47920);
+
+static const Color lightTextGrey = Color(0xff8b838c);
+
+static const Color black = Color(0xff000000);
+
+static const Color orange = Color(0xfff2994a);
+}
